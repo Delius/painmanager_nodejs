39c415828cfdd484b8f8e669abd4c141c079aaec
ismc2222 <pawel@fibrotrust.org>
2014-10-27 19:35:54 +0000

    added main region and data

--- server.js
+++ server.js
@@ -65,24 +65,33 @@ if ('development' == env) {
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error...'));
 db.once('open', function callback() {
-    console.log('multivision db opened');
+    console.log('PainManager db opened');
 });
-var messageSchema = mongoose.Schema({message: String});
-var Message = mongoose.model('Message', messageSchema);
-var mongoMessage;
-Message.findOne().exec(function(err, messageDoc) {
-    mongoMessage = messageDoc.message;
-});
+
+//Message from mongo
+
+//var messageSchema = mongoose.Schema({message: String});
+//var Message = mongoose.model('Message', messageSchema);
+//var mongoMessage;
+//Message.findOne().exec(function(err, messageDoc) {
+//    mongoMessage = messageDoc.message;
+//});
 
 app.get('/partials/:partialPath', function(req, res) {
     res.render('partials/' + req.params.partialPath);
 });
 
 app.get('*', function(req, res) {
-    res.render('index', {
-        mongoMessage: mongoMessage
-    });
+    res.render('index');
 });
+
+//Message from mongo
+
+//app.get('*', function(req, res) {
+//    res.render('index', {
+//        mongoMessage: mongoMessage
+//    });
+//});
 
 var port = process.env.PORT || 3030;
 app.listen(port);
