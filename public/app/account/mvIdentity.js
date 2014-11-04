f0c147ac99423143462a5f27fcede60fcfdf94ee
Delius <pawel@fibrotrust.org>
2014-11-04 14:34:24 +0000

    made user persistent

--- public/app/account/mvIdentity.js
+++ public/app/account/mvIdentity.js
@@ -1,11 +1,11 @@
-angular.module('app').factory('mvIdentity', function() {
-    //var currentUser;
-    //if(!!$window.bootstrappedUserObject) {
-    //    currentUser = new mvUser();
-    //    angular.extend(currentUser, $window.bootstrappedUserObject);
-    //}
+angular.module('app').factory('mvIdentity', function($window) {
+    var currentUser;
+    if(!!$window.bootstrappedUserObject) {
+        currentUser = $window.bootstrappedUserObject;
+        //angular.extend(currentUser, $window.bootstrappedUserObject);
+    }
     return {
-        currentUser: undefined,
+        currentUser: currentUser,
         isAuthenticated: function() {
             return !!this.currentUser;
         }
