e4db968701fb489490166a4baa11043c777d3577
Delius <pawel@fibrotrust.org>
2014-11-05 19:19:22 +0000

    :

--- server/mdels/User.js
+++ server/mdels/User.js
@@ -26,24 +26,24 @@ userSchema.methods = {
 var User =  mongoose.model('User', userSchema);
 
 
-function createDefaultUsers(){
-    User.find({}).exec(function(err, collection) {
-        if (collection.length === 0) {
-            var salt, hash;
-
-            salt = encrypt.createSalt();
-            hash = hashPwd(salt, 'ismc2222');
-            User.create({firstName:'Pawel', lastName:'Jakubowski', username:'ismc2222',salt:salt,hashed_pwd:hash,roles:['admin']});
-
-            salt = encrypt.createSalt();
-            hash = hashPwd(salt, 'palomino');
-            User.create({firstName:'Ewa', lastName:'Palomino', username:'palomino',salt:salt,hashed_pwd:hash,roles:[]});
-
-            salt = encrypt.createSalt();
-            hash = hashPwd(salt, 'frank');
-            User.create({firstName:'Frank', lastName:'Knight', username:'frank',salt:salt,hashed_pwd:hash});
-        }
-    })
-};
+//function createDefaultUsers(){
+//    User.find({}).exec(function(err, collection) {
+//        if (collection.length === 0) {
+//            var salt, hash;
+//
+//            salt = encrypt.createSalt();
+//            hash = hashPwd(salt, 'ismc2222');
+//            User.create({firstName:'Pawel', lastName:'Jakubowski', username:'ismc2222',salt:salt,hashed_pwd:hash,roles:['admin']});
+//
+//            salt = encrypt.createSalt();
+//            hash = hashPwd(salt, 'palomino');
+//            User.create({firstName:'Ewa', lastName:'Palomino', username:'palomino',salt:salt,hashed_pwd:hash,roles:[]});
+//
+//            salt = encrypt.createSalt();
+//            hash = hashPwd(salt, 'frank');
+//            User.create({firstName:'Frank', lastName:'Knight', username:'frank',salt:salt,hashed_pwd:hash});
+//        }
+//    })
+//};
 
 exports.createDefaultUsers = createDefaultUsers;
